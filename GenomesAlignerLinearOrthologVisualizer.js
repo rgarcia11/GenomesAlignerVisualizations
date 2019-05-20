492319775aea426aeca041c6e9f951ac5e18e0a0
Rogelio Garcia <r.garcia11@uniandes.edu.co>
2019-05-20 01:24:23 -0500

    Removed slider until it is ready

--- GenomesAlignerLinearOrthologVisualizer.js
+++ GenomesAlignerLinearOrthologVisualizer.js
@@ -24,36 +24,7 @@ option.append('input').attr('id', 'MinimumChromosomeLengthTextInput').attr('type
 option.append('input').attr('id', 'MinimumChromosomeLengthButton').attr('type', 'button').attr('value', `ChromosomeLength`);
 d3.select('#MinimumChromosomeLengthButton').on('click', chromosomeLength);
 
-option.append('div').attr('id', 'value-step');
-option.append('div').attr('id', 'slider-step');
 
-var data = [0, 0.005, 0.01, 0.015, 0.02, 0.025];
-
-// Step
-var sliderStep = d3
-    .sliderBottom()
-    .min(d3.min(data))
-    .max(d3.max(data))
-    .width(300)
-    .tickFormat(d3.format('.2%'))
-    .ticks(5)
-    .step(0.005)
-    .default(0.015)
-    .on('onchange', val => {
-        d3.select('p#value-step').text(d3.format('.2%')(val));
-    });
-
-var gStep = d3
-    .select('div#slider-step')
-    .append('svg')
-    .attr('width', 500)
-    .attr('height', 100)
-    .append('g')
-    .attr('transform', 'translate(30,30)');
-
-gStep.call(sliderStep);
-
-d3.select('p#value-step').text(d3.format('.2%')(sliderStep.value()));
 const canvasContainer = option.append('div').attr('class', 'container');
 canvasContainer.append('div').attr('class', 'canvas');
 
